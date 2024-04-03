ccf672908b58ce0f233a1f72ba1e2e44da6d5d2f
Alpha Chen <alpha@kejadlen.dev>
2024-04-02 19:51:05 -0700

    more stats

--- stats.rb
+++ stats.rb
@@ -11,17 +11,49 @@ def data
   end
 end
 
-solve_times = data.each.with_object(Hash.new {|h,k| h[k] = [] }) {|(date, datum), times|
-  next unless solve_time = datum.fetch("calcs", {}).fetch("secondsSpentSolving", nil)
-  times[date.year] << solve_time
-}
+solve_times = data.to_h
+  .transform_values { _1.fetch("calcs", {}).fetch("secondsSpentSolving", nil) }
+  .reject { _2.nil? }
 
-solve_times.each do |year, times|
-  total = times.sum
+# solve_times
+#   .group_by {|date,_| date.year }
+#   .transform_values { _1.map(&:last) }
+#   .each do |year, times|
+#     total = times.sum
 
+#     m, s = total.divmod(60)
+#     h, m = m.divmod(60)
+#     [h, m, s]
+
+#     puts "%d: %02d:%02d:%02d" % [year, h, m, s]
+#   end
+
+(0..6).each do |wday|
+  puts "SMTWTFS"[wday]
+    solve_times
+      .select {|date,_| date.wday == wday }
+      .group_by {|date,_| date.year }
+      .transform_values { _1.map(&:last) }
+      .each do |year, times|
+        total = times.sum / times.size
+
+        m, s = total.divmod(60)
+        h, m = m.divmod(60)
+        [h, m, s]
+
+        puts "%d: %02d:%02d:%02d" % [year, h, m, s]
+      end
+  puts
+end
+
+(0..6).each do |wday|
+  times = solve_times
+    .select {|date,_| date.wday == wday }
+    .values
+  total = times.sum / times.size
   m, s = total.divmod(60)
   h, m = m.divmod(60)
   [h, m, s]
 
-  puts "%d: %02d:%02d:%02d" % [year, h, m, s]
+  puts "%s: %02d:%02d:%02d" % ["SMTWTFS"[wday], h, m, s]
 end
